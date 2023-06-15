427f59a97bd1359bfeb1a6bc3739f41d55241c3d
Arne de Beer <a.r.debeer@student.tudelft.nl>
2023-06-15 17:49:10 +0200

    Add separate file for model conversion to TFLite

--- Model/model_convertor.py
+++ Model/model_convertor.py
@@ -2,4 +2,31 @@
 # It will have various functions for converting models with different optimisations
 
 import tensorflow as tf
+import numpy as np
 
+def quantize_model( model, representative_data, write_to_file = False): 
+    converter = tf.lite.TFLiteConverter.from_keras_model(model)
+    converter.optimizations = [tf.lite.Optimize.DEFAULT]
+
+    def representative_data_generator():
+        for i in range(min(1000, len(representative_data))):
+            sample_data = representative_data[i]
+            sample_data = np.expand_dims(sample_data, axis=0)
+            yield [sample_data.astype(np.float32)]
+
+    converter.representative_dataset = representative_data_generator
+
+    converter.target_spec.supported_ops = [tf.lite.OpsSet.TFLITE_BUILTINS_INT8]
+    # converter._experimental_new_quantizer = True
+    converter.allow_custom_ops = True
+    # converter.target_spec.supported_types = [tf.int8]
+
+    # converter.inference_input_type = tf.float32
+    # converter.inference_output_type = tf.uint8
+
+    tflite_model = converter.convert()
+
+    if write_to_file:
+        open("converted_model.tflite", "wb").write(tflite_model)
+
+    return tflite_model
